2797b3c00becd87669692c4a1925ae9e2ac2392e
Matyko <nincs.matyas@gmail.com>
2018-03-16 17:08:03 +0100

    Startup

---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+
+  data () {
+    return {
+      msg: 'Welcome to your Vue.js powered Spring Boot App'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
